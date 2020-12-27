6418c6e9d1cd172bae53cd248a97b8ace84117f5
Chandra kishor Gauro <ckgauro@gmail.com>
2020-12-27 15:28:53 -0500

    Ch02 Compelted

---
+++ ch03/tacos-jdbc/src/main/java/com/gauro/TacoJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.gauro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoJdbcApplication.class, args);
+	}
+
+}
